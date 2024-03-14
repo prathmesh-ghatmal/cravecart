d048976ae2324870fa5920084c03da36c62ff3e9
prathmesh-ghatmal <mrjpg2311@gmail.com>
2024-03-14 13:14:25 +0530

    login page done

--- app/src/main/java/com/example/cravecart/start_activity.kt
+++ app/src/main/java/com/example/cravecart/start_activity.kt
@@ -1,11 +1,20 @@
 package com.example.cravecart
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cravecart.databinding.ActivityStartBinding
 
 class start_activity : AppCompatActivity() {
+    private val binding:ActivityStartBinding by lazy {
+        ActivityStartBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
+        setContentView(binding.root)
+        binding.nextButton.setOnClickListener{
+            val intent=Intent(this,login_activity::class.java)
+            startActivity(intent)
+        }
     }
 }
