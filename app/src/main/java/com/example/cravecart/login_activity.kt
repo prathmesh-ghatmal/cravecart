d738aa6dff3abf5def60d02dda3f3c25d797978a
prathmesh-ghatmal <mrjpg2311@gmail.com>
2024-03-14 14:12:21 +0530

    sign up page done

--- app/src/main/java/com/example/cravecart/login_activity.kt
+++ app/src/main/java/com/example/cravecart/login_activity.kt
@@ -1,11 +1,28 @@
 package com.example.cravecart
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cravecart.databinding.ActivityLoginBinding
+import com.example.cravecart.databinding.ActivityStartBinding
 
 class login_activity : AppCompatActivity() {
+ private val binding:ActivityLoginBinding by lazy {
+     ActivityLoginBinding.inflate(layoutInflater)
+ }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+       binding.loginbutton1.setOnClickListener{
+           val intent=Intent(this,Signin_Activity::class.java)
+           startActivity(intent)
+       }
+
+        binding.Donthavebutton.setOnClickListener{
+            val intent=Intent(this,Signin_Activity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
