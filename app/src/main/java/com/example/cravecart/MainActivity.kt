85a14ef674a8b4d3f0072704195aeca6df3adb11
prathmesh-ghatmal <mrjpg2311@gmail.com>
2024-03-31 07:27:04 +0530

    user side frontend finalized

--- app/src/main/java/com/example/cravecart/MainActivity.kt
+++ app/src/main/java/com/example/cravecart/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.cravecart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
@@ -13,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         var NavController=findNavController(R.id.fragmentContainerView)
